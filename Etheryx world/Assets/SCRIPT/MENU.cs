a21861b1fb0dcd57215947faaafb26d1672872e3
Line Stoff <line.leclercq-soethoff@epita.fr>
2024-01-21 20:34:26 +0100

    main menu 1

---
+++ Etheryx world/Assets/SCRIPT/MENU.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENU : MonoBehaviour
+{
+ 
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void SettingsButton()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
