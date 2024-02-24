8403b3983603704f7bab5fdfc80f399b28c35c9e
arwen <arwen.grandais@epita.fr>
2024-02-24 14:28:31 +0100

    mouvments

---
+++ Etheryx world/Assets/perso_principal.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perso_principal : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 dir;
+    
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
+    }
+}
