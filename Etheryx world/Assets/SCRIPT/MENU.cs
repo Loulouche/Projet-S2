106421e5f2c1f09d22f9a90282d13b6b6a43f4c0
Line Stoff <lineleso@gmail.com>
2024-02-26 20:41:55 +0100

    boutonpause01

--- Etheryx world/Assets/SCRIPT/MENU.cs
+++ Etheryx world/Assets/SCRIPT/MENU.cs
@@ -6,35 +6,35 @@ using UnityEngine.UI;
 
 public class MENU : MonoBehaviour
 {
- 
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
- 
- 
-
     public void SettingsButton()
     {
 
     }
-
     public void QuitGame()
     {
         Application.Quit();
     }
-
-
-
     public void GoToMenu()
     {
         SceneManager.LoadSceneAsync(0);
     }
 
+public void Pause()
+{
+Time.timeScale = 0;
 
-    
+}
 
+public void Resume()
+{
+Time.timeScale = 1;
 
+}
 
 
 
---
+++ Etheryx world/Assets/SCRIPT/perso_principal.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class perso_principal : MonoBehaviour
+{
+    
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    private Vector2 mouvement;
+    public Animator animator;
+    
+    
+    void Update()
+    {
+        mouvement.x = Input.GetAxisRaw("Horizontal");
+            mouvement.y = Input.GetAxisRaw("Vertical");
+
+            animator.SetFloat("Horizontal", mouvement.x);
+            animator.SetFloat("Vertical", mouvement.y);
+            animator.SetFloat("Speed", mouvement.magnitude);
+        
+        
+            rb.MovePosition(rb.position + mouvement * speed * Time.deltaTime);
+
+
+        
+    }
+}
