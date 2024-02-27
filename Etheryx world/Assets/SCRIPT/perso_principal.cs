a5834e2931e12f5eb74c940353aa4afc7aec48e0
Line Stoff <lineleso@gmail.com>
2024-02-27 12:04:54 +0100

    perso

--- Etheryx world/Assets/SCRIPT/perso_principal.cs
+++ Etheryx world/Assets/SCRIPT/perso_principal.cs
@@ -11,31 +11,38 @@ public class perso_principal : MonoBehaviour
     public float speed = 5f;
     private Vector2 mouvement;
     public Animator animator;
+public bool EpeeActive = false;
 
-    public Collider2D collision;
+
+
     
     
     void Update()
     {
         mouvement.x = Input.GetAxisRaw("Horizontal");
-            mouvement.y = Input.GetAxisRaw("Vertical");
+        mouvement.y = Input.GetAxisRaw("Vertical");
 
-            animator.SetFloat("Horizontal", mouvement.x);
-            animator.SetFloat("Vertical", mouvement.y);
-            animator.SetFloat("Speed", mouvement.magnitude);
+
+            if (!EpeeActive)
+            {
+                animator.SetFloat("Horizontal", mouvement.x);
+                animator.SetFloat("Vertical", mouvement.y);
+                animator.SetFloat("Speed", mouvement.magnitude);
+            }
+            
         
         
             rb.MovePosition(rb.position + mouvement * speed * Time.deltaTime);
-
-
-        
     }
 
+    
+    void RamasserEpee()
+    
+{
+EpeeActive = true;
 
-    public void OnTriggerEnter2D(Collider2D collision)
-    { 
-        SceneManager.LoadSceneAsync(2); 
-    }
+}
+
 
 
 }
