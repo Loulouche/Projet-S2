6c6ec76d7ee30a45b629cbfbf6b54a23cb00cd37
Loulouche <louise.delarue20@gmail.com>
2024-05-14 23:17:11 +0200

    integration multi

---
+++ Etheryx world/Assets/SCRIPT/MenuController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private string VersionName = "0.1";
+    [SerializeField] private GameObject UsernameMenu;
+    [SerializeField] private GameObject ConnectPanel;
+
+    [SerializeField] private TMP_InputField UsernameInput;
+    [SerializeField] private TMP_InputField CreateGameInput;
+    [SerializeField] private TMP_InputField JoinGameInput;
+    
+    [SerializeField] private GameObject StartButton;
+    
+    private void Start()
+    {
+        UsernameMenu.SetActive(true);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+    
+    public void ChangeUserNameInput()
+    {
+        if (UsernameInput.text.Length >= 3)
+        {
+            StartButton.SetActive(true);
+        }
+        else
+        {
+            StartButton.SetActive(false);
+        }
+    }
+    
+    public void SetUserName()
+    {
+        UsernameMenu.SetActive(false);
+        PhotonNetwork.NickName = UsernameInput.text;
+    }
+}
