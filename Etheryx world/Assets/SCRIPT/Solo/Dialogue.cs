d2488c5a78ce84b3bc955b4e3fc98d78a132797d
Line Stoff <lineleso@gmail.com>
2024-05-22 22:40:34 +0200

    perso_qui_parle

---
+++ Etheryx world/Assets/SCRIPT/Solo/Dialogue.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public GameObject Gamemanager;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+    private bool isrange;
+    private bool isDialogueStarted;
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        Gamemanager.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isrange && !isDialogueStarted)
+        {
+            Gamemanager.SetActive(true);
+            StartDialogue();
+            isDialogueStarted = true;
+            Debug.Log("Dialogue started");
+        }
+
+        if (isDialogueStarted && Input.GetKeyDown(KeyCode.Return))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    void EndDialogue()
+    {
+        Gamemanager.SetActive(false);
+        textComponent.text = string.Empty;
+        isDialogueStarted = false;
+        Debug.Log("Dialogue ended");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isrange = true;
+            Debug.Log("Player entered range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isrange = false;
+            if (isDialogueStarted)
+            {
+                EndDialogue();
+            }
+            Debug.Log("Player exited range");
+        }
+    }
+}
